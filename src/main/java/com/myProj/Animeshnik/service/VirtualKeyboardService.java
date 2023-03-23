6db23afb05e8f15cf430a6b95f40cc7d4f5dc49e
kolgotik <90344391+kolgotik@users.noreply.github.com>
2023-03-23 18:54:56 +0500

    added command that activates command keyboard

--- src/main/java/com/myProj/Animeshnik/service/VirtualKeyboardService.java
+++ src/main/java/com/myProj/Animeshnik/service/VirtualKeyboardService.java
@@ -3,5 +3,5 @@ package com.myProj.Animeshnik.service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 public interface VirtualKeyboardService {
-    void sendGeneralVirtualCommandKeyboard(SendMessage sendMessage);
+    void sendGeneralVirtualCommandKeyboardWithMessage(SendMessage sendMessage);
 }
